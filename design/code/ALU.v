e44f909afdc2ea9b202824b7cc94196d1885c333
abirfan <abirfan@uwaterloo.ca>
2025-09-26 00:50:25 -0400

    ALU operations performed, 2x1mux created

--- design/code/ALU.v
+++ design/code/ALU.v
@@ -6,4 +6,22 @@ module ALU(
 );
 
 
+always@(*)begin
+    alu_result = 32'b0;
+    case(alu_sel)
+        4'b0000: alu_result = src_a + src_b;
+        4'b0001: alu_result = src_a - src_b;
+        4'b0010: alu_result = src_a & src_b;
+        4'b0011: alu_result = src_a | src_b;
+        4'b0100: alu_result = src_a ^ src_b;
+        4'b0101: alu_result = {31'b0, ($signed(src_a)<$signed(src_b))};
+        4'b0110: alu_result = {31'b0, (src_a<src_b)};
+        4'b0111: alu_result = src_a<<src_b[4:0];
+        4'b1000: alu_result = src_a>>src_b[4:0];
+        4'b1001: alu_result = $signed(src_a)>>>src_b[4:0];
+        4'b1111: alu_result = src_b;
+        default: alu_result = 32'b0;
+    endcase
+end
+
 endmodule
